686c884fd5a3e4f9ebed5d986dc33c627c6642f2
moxiaogui0301 <361941176@qq.com>
2019-11-04 17:48:13 +0800

    mytomcat3 1.使用门面模式保护Request和Response 2.servlet输出需按照HTTP响应协议格式 3.Request和Response通过继承ServletRequest和ServletResponse实现

---
+++ src/com/dengxin/mytomcat3/Constants.java
@@ -0,0 +1,7 @@
+package com.dengxin.mytomcat3;
+
+import java.io.File;
+
+public class Constants {
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+}
